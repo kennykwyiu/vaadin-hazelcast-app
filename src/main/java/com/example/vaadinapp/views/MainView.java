75de54c569daf27eb8c3ec88f62960e6a362f1da
kennyyiu <kenny.kwyiu@gmail.com>
2025-08-16 17:14:45 +0800

    change route

--- src/main/java/com/example/vaadinapp/views/MainView.java
+++ src/main/java/com/example/vaadinapp/views/MainView.java
@@ -6,16 +6,18 @@ import com.vaadin.flow.component.html.H1;
 import com.vaadin.flow.component.html.Paragraph;
 import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.page.Push;
 import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.router.Route;
 import com.vaadin.flow.server.VaadinRequest;
 import com.vaadin.flow.server.VaadinService;
+import com.vaadin.flow.shared.communication.PushMode;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
-@Route("")
+@Route("main")
 public class MainView extends VerticalLayout {
 
     private final Div sessionInfo = new Div();
