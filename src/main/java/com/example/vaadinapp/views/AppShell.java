0850701c07ea70d0266026d80a4da6d58a3fe776
kennyyiu <kenny.kwyiu@gmail.com>
2025-08-16 17:14:37 +0800

    add shell

---
+++ src/main/java/com/example/vaadinapp/views/AppShell.java
@@ -0,0 +1,11 @@
+//package com.example.vaadinapp.views;
+//
+//import com.vaadin.flow.component.page.AppShellConfigurator;
+//import com.vaadin.flow.component.page.Push;
+//import com.vaadin.flow.server.PWA;
+//import com.vaadin.flow.shared.communication.PushMode;
+//
+//
+//@PWA(name = "Vaadin App", shortName = "App")
+//public class AppShell implements AppShellConfigurator {
+//}
